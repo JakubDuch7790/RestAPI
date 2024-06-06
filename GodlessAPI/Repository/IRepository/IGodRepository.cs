c6ee890dd0ce2415d2cfd218765853337f57cfbb
Jakub Duch <jakub.duch23@gmail.com>
2024-06-06 13:26:52 +0200

    Repository and its interface added

---
+++ GodlessAPI/Repository/IRepository/IGodRepository.cs
@@ -0,0 +1,18 @@
+﻿using GodlessAPI.Models;
+using System.Linq.Expressions;
+
+namespace GodlessAPI.Repository.IRepository
+{
+    public interface IGodRepository
+    {
+        Task Create(Godless god);
+        Task<List<Godless>> GetAll(Expression<Func<Godless>> filter = null);
+        Task<Godless> Get(Expression<Func<Godless>> filter = null, bool tracked = true);
+
+        Task Save();
+        Task Remove(Godless god);
+
+
+
+    }
+}
