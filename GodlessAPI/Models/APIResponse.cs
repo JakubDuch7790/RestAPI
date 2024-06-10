ba7c04e3b4f1f6750c16b263ce22a13909fa7aad
Jakub Duch <jakub.duch23@gmail.com>
2024-06-10 12:55:49 +0200

    API Response class added

---
+++ GodlessAPI/Models/APIResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace GodlessAPI.Models;
+
+public class APIResponse
+{
+    public HttpStatusCode StatusCode { get; set; }
+    public bool IsSuccessful { get; set; }
+    public List<string> ErrorMessages { get; set; }
+    public object Result { get; set; }
+}
